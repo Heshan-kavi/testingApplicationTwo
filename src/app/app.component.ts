43f58deca7eadd09242322f5fbd265aca284de40
Heshan-kavi <kavindaheshan96@gmail.com>
2021-10-22 16:51:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testing application by Heshan Kavinda';
+  title = 'testing two application by Heshan Kavinda';
 }
